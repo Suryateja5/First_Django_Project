b0bb0aa3a1bcc5a6f288a58125546f696f2f81d7
Suryateja5 <112101284+Suryateja5@users.noreply.github.com>
2022-09-20 23:43:42 -0500

    adding files

--- localLibrary/settings.py
+++ localLibrary/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$-t^a)q1n(!o3#-hkx-0h(f09qfc-ao7#bfmlo&__e=wlw7v)c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
